d88c4abfec629975327309c95d65795a4b4b3637
Markus Bacher <qwertzuiop_581@msn.com>
2017-04-13 14:25:35 +0200

    reading them blobs

--- js/dsp/websocketclient.js
+++ js/dsp/websocketclient.js
@@ -1,7 +1,7 @@
 var exampleSocket = null;
 
 function startWebsocket(){
-    exampleSocket = new WebSocket("ws://127.0.0.1:12345", "WebSDR");
+    exampleSocket = new WebSocket("ws://127.0.0.1:44444", "binary");
     exampleSocket.onerror = connectionRefused;
     exampleSocket.onopen = connectionOpened;
 }
@@ -13,14 +13,28 @@ function sendMessage(msg){
 }
 
 function readMessage (event) {
+
+
+
+
+    console.log("Server:" + event.data.size);
     var myReader = new FileReader();
+    var arrBuff;
+    myReader.onload = function() {
+        console.log("Result: " + this.result);
+        arrBuff = this.result;
+
+        console.log("ArrBuff=" + String.fromCharCode.apply(null, new Uint8Array(arrBuff)));
+    };
+
     //handler executed once reading(blob content referenced to a variable) from blob is finished.
     myReader.addEventListener("loadend", function(e){
-        console.log("Server:" + e.srcElement.result);
+        console.log("Server: " + JSON.stringify(e));
         //document.getElementById("paragraph").innerHTML = e.srcElement.result;//prints a string
     });
     //start the reading process.
-    myReader.readAsText(event.data);
+    myReader.readAsArrayBuffer(event.data);
+
 
 
 }
